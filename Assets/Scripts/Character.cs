f34ddcf885be7d821fe9e92274be32ba4c8ef881
Akenthor Pendragon <akenpendragon@gmail.com>
2020-12-12 05:10:39 +0300

    Cold and hot zone hp and size increase\decrease added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int MAX_HEALTH = 100;
+    const float MAX_SIZE = 1.0f;
+    private int _health = 100;
+    private bool _isInColdZone = false;
+    private float _size = 1.0f;
+
+    public int Health { get { return _health; } }
+    public bool IsInColdZone { get { return _isInColdZone; } set { _isInColdZone = value; } }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -20,4 +27,43 @@ public class Character : MonoBehaviour
     {
         transform.Translate(movement, Space.World);
     }
+
+    public void IncreaseHealth(int amount)
+    {
+        _health = Mathf.Clamp(_health + amount, 0, MAX_HEALTH);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _health = Mathf.Clamp(_health - amount, 0, MAX_HEALTH);
+    }
+
+    public void IncreaseSize(float amount)
+    {
+        _size = Mathf.Clamp(_size + amount, 0, MAX_SIZE);
+        transform.localScale = new Vector3(_size, _size, _size);
+    }
+
+    public void DecreaseSize(float amount)
+    {
+        _size = Mathf.Clamp(_size - amount, 0, MAX_SIZE);
+        transform.localScale = new Vector3(_size, _size, _size);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Cold zone"))
+        {
+            _isInColdZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Cold zone"))
+        {
+            _isInColdZone = false;
+        }
+    }
+
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private Character _player;
+    private bool _isGameRunning = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = (Character)FindObjectOfType(typeof(Character));
+
+        // 
+        StartCoroutine("PlayerInZoneHealthChange");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player.Health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    IEnumerator PlayerInZoneHealthChange()
+    {
+        while (_isGameRunning)
+        {
+            if (_player.IsInColdZone)
+            {
+                _player.IncreaseHealth(10);
+                _player.IncreaseSize(0.1f);
+            }
+            else
+            {
+                _player.DecreaseHealth(1);
+                _player.DecreaseSize(0.01f);
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void GameOver()
+    {
+        // Temporary
+        Time.timeScale = 0;
+    }
+
+
+}
