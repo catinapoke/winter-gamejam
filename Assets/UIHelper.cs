e7f82be7599ececfc5c58b9590abdf046de3e7eb
DaBoi <mihansp1@gmail.com>
2020-12-13 15:31:33 +0300

    Menu

---
+++ Assets/UIHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHelper : MonoBehaviour
+{
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene(GameHelper.MAIN_SCENE_NAME);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
