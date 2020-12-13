28fe74090d7686018846165f9074ff2d2095c619
Akenthor Pendragon <akenpendragon@gmail.com>
2020-12-13 17:18:21 +0300

    Pre-final push

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource audioSource;
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+}
