83dbb7193c19e685dfb1b0ebc59c3545d4b88623
DaBoi <mihansp1@gmail.com>
2020-12-13 16:37:19 +0300

    Object spawner

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private Vector3 _position;
+
+    public void Spawn()
+    {
+        Instantiate(_prefab, _position, Quaternion.identity);
+    }
+}
